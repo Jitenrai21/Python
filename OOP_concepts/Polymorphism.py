eddbc759a20c7d52ac1de939f3e1a984f40e08c7
Jiten Rai <raijiten96@gmail.com>
2024-11-09 22:42:42 +0545

    polymorphism

--- OOP_concepts/Polymorphism.py
+++ OOP_concepts/Polymorphism.py
@@ -1,23 +1,42 @@
-class nepal:
-    def country(self):
-        print('This is Nepal.')
-    def lang(self):
-        print('Nepalese language is the national language.')
-    def cond(self):
-        print('It is a developing country.')
+#method overriding
+# class nepal:
+#     def country(self):
+#         print('This is Nepal.')
+#     def lang(self):
+#         print('Nepalese language is the national language.')
+#     def cond(self):
+#         print('It is a developing country.')
 
-class USA:
-    def country(self):
-        print('This is USA.')
-    def lang(self):
-        print("English is the native language.")
-    def cond(self):
-        print('USA is a developed country.')
+# class USA:
+#     def country(self):
+#         print('This is USA.')
+#     def lang(self):
+#         print("English is the native language.")
+#     def cond(self):
+#         print('USA is a developed country.')
 
-n1 = nepal()
-u1 = USA()
+# n1 = nepal()
+# u1 = USA()
 
-for obj in (n1, u1):
-    obj.country()
-    obj.lang()
-    obj.cond()
+# for obj in (n1, u1):
+#     obj.country()
+#     obj.lang()
+#     obj.cond()
+
+
+#polymorphism by function
+class dog:
+    def sound(self):
+        return 'Dog is barking.'
+
+class cat:
+    def sound(self):
+        return 'Cat is meowing.'
+
+def makeSound(animal):
+    return animal.sound()
+
+animals = [dog(), cat()]
+
+for animal in animals:
+    print(makeSound(animal))
