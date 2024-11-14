088513a877eeeb4d5bd59a81ab7044b5b8b5b7ad
Jiten Rai <raijiten96@gmail.com>
2024-11-14 21:38:56 +0545

    basic list

--- ListPrograms/Basics.py
+++ ListPrograms/Basics.py
@@ -3,23 +3,52 @@ lst = [21,31,41,51,61]
 lst[1], lst[2] = lst[2], lst[1]
 print(lst)
 
-count = 0
-for val in lst:
-    count +=1
-print(f"Length of the given list is: {count}")
+# count = 0
+# for val in lst:
+#     count +=1
+# print(f"Length of the given list is: {count}")
 
-#searching elements in list
-lst1 = [21,31,41,51,61]
-key = int(input("Enter the desired number to searched: "))
-flag = False
-i = 1
-for val in lst1:
-    if val != key:
-        i += 1
-    elif val == key:
-        flag = True
-        break
-if flag:
-    print(f"The looked for element is in the list located at {i}th position.")
-else:
-    print("The looked for element is not in the list.")
+# #searching elements in list
+# lst1 = [21,31,41,51,61]
+# key = int(input("Enter the desired number to searched: "))
+# flag = False
+# i = 1
+# for val in lst1:
+#     if val != key:
+#         i += 1
+#     elif val == key:
+#         flag = True
+#         break
+# if flag:
+#     print(f"The looked for element is in the list located at {i}th position.")
+# else:
+#     print("The looked for element is not in the list.")
+
+# #Smallest in the list
+# smallest = lst[0]
+# for val in lst:
+#     if val < smallest:
+#         smallest = val
+# print(f"Smallest element is {smallest}.")
+
+#by sorting
+# lst.sort()
+# smallest = lst[0]
+# print(smallest)
+
+#second greatest number
+num = sorted(lst)[-2]
+print(num)
+
+#n number of greatest numbers
+def NGrestesNumbers(lst, n):
+    rev = []
+    for i in range(n):
+        max = 0
+        for j in range(len(lst)):
+            if lst[j] > max:
+                max = lst[j]
+        lst.remove(max)
+        rev.append(max)
+    print(rev)
+NGrestesNumbers(lst, 2)
