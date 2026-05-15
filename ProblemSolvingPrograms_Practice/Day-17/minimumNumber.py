b16928e12b84af4dd7c245cf1c86531641150a18
Jiten Rai <raijiten96@gmail.com>
2026-05-15 22:47:17 +0545

    44m

---
+++ ProblemSolvingPrograms_Practice/Day-17/minimumNumber.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'minimumNumber' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. STRING password
+#
+
+def minimumNumber(n, password):
+    # Return the minimum number of characters to make the password strong
+    numbers = "0123456789"
+    lower_case = "abcdefghijklmnopqrstuvwxyz"
+    upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    special_characters = "!@#$%^&*()-+"
+    missing = 0
+    if not any(char in numbers for char in password):
+        missing += 1
+    if not any(char in lower_case for char in password):
+        missing += 1
+    if not any(char in upper_case for char in password):
+        missing += 1
+    if not any(char in special_characters for char in password):
+        missing += 1
+    return max(missing, 6-n)
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    password = input()
+
+    answer = minimumNumber(n, password)
+
+    fptr.write(str(answer) + '\n')
+
+    fptr.close()
