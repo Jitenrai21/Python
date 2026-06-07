2a5493c6f112c8f01f52b76d47a9cf496899a47f
Jiten Rai <raijiten96@gmail.com>
2026-06-07 21:23:46 +0545

    20m

---
+++ ProblemSolvingPrograms_Practice/Day-22/gameOfThrones.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+#
+# Complete the 'gameOfThrones' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def gameOfThrones(s):
+    # Write your code here
+    counts =  Counter(s)
+    odds = 0
+    for value in counts.values():
+        if value%2 != 0:
+            odds += 1
+    if odds <= 1:
+        return 'YES'
+    else:
+        return 'NO'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = gameOfThrones(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
---
+++ ProblemSolvingPrograms_Practice/Day-22/makingAnagrams.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'makingAnagrams' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. STRING s1
+#  2. STRING s2
+#
+
+def makingAnagrams(s1, s2):
+    # Write your code here
+    temp = [0] * 26
+    for char in s1:
+        temp[ord(char)-ord('a')] += 1
+    for char in s2:
+        temp[ord(char)-ord('a')] -= 1
+    return sum(abs(count) for count in temp)
+            
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s1 = input()
+
+    s2 = input()
+
+    result = makingAnagrams(s1, s2)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
