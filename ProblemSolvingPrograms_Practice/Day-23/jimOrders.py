b61a3cdbdfb8b61c8baf04b5b89a17d9feb27253
Jiten Rai <raijiten96@gmail.com>
2026-06-22 22:07:17 +0545

    31m

---
+++ ProblemSolvingPrograms_Practice/Day-23/jimOrders.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'jimOrders' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts 2D_INTEGER_ARRAY orders as parameter.
+#
+
+def jimOrders(orders):
+    # Write your code here
+    time = []
+    for i, (order, prep) in enumerate(orders):
+        delivery_time = order+ prep
+        customer_id = i + 1
+        time.append((delivery_time, customer_id))
+    time.sort()
+    res = [customer_id for delivery_time, customer_id in time]
+    return res
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    orders = []
+
+    for _ in range(n):
+        orders.append(list(map(int, input().rstrip().split())))
+
+    result = jimOrders(orders)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
